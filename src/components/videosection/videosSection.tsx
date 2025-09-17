942b153c34ef71f983d3e30ed72e02c5cf1ff246
Wutachi <victorhdchagas@live.com>
2025-09-17 19:24:13 -0300

    layout adjust

--- src/components/videosection/videosSection.tsx
+++ src/components/videosection/videosSection.tsx
@@ -5,13 +5,15 @@ export default async function VideoSection() {
   const videos = await getVideos()
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 mx-auto gap-8 md:gap-2 container overflow-auto min-h-1/2 max-h-1/2">
+    <section className="flex flex-col mx-auto gap-8 md:gap-2 container overflow-auto min-h-1/2 max-h-1/2">
       <span className="text-yellow-500 text-4xl mx-auto py-4 font-bold">
         Ultimos vídeos
       </span>
-      {videos.map((video) => (
-        <VideoCard video={video} key={video.videoId} />
-      ))}
-    </div>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-8 md:gap-2 ">
+        {videos.map((video) => (
+          <VideoCard video={video} key={video.videoId} />
+        ))}
+      </div>
+    </section>
   )
 }
